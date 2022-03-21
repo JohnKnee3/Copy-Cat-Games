818c4266cfb68c21e0d0c3a49940bb7d2c750a06
john.a.clark3 <john.a.clark3@gmail.com>
2022-03-21 17:24:15 -0600

    Tested more articles with seeds

--- seeds/article_commentData.js
+++ seeds/article_commentData.js
@@ -21,6 +21,28 @@ const article_commentData = [
     article_id: 2,
     user_id: 2,
   },
+  {
+    comment_text: "This is my favoritest game ever!!!!!",
+    article_id: 1,
+    user_id: 1,
+  },
+  {
+    comment_text: "Meh it could be better.  Calm down Molly",
+    article_id: 1,
+    user_id: 2,
+  },
+  {
+    comment_text:
+      "I used HATE this game, but now that CCG claims they make it it's one of my top 10 favorite games of all time.",
+    article_id: 3,
+    user_id: 1,
+  },
+  {
+    comment_text:
+      "I love how this game uses love to love the love.  You know what I mean love love?",
+    article_id: 3,
+    user_id: 3,
+  },
 ];
 
 const seedArticleComments = () =>
