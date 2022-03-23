6f9070aaa099a3a3c159f19be8d3d7bf3a120f19
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-03-23 11:22:27 -0600

    render forum

--- controllers/forum-home-routes.js
+++ controllers/forum-home-routes.js
@@ -31,7 +31,7 @@ router.get('/', (req, res) => {
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
 
-            res.render('homepage', {
+            res.render('forum', {
                 posts,
                 loggedIn: req.session.loggedIn
             });
