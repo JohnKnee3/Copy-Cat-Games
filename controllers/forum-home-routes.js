36ec999afef03374e038461b45d3995218004b54
BullMooseDev <koleervine@gmail.com>
2022-03-23 12:47:33 -0600

    moving to store

--- controllers/forum-home-routes.js
+++ controllers/forum-home-routes.js
@@ -1,21 +1,21 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Forum_Post, User, Forum_Comment } = require('../models');
+const { Forum_Post, User, Forum_Comment, Forum_Vote } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
     console.log('======================');
-    Forum_Post.findAll({
-        // attributes: [
-        //     'id',
-        //     'post_content',
-        //     'title',
-        //     'created_at',
-        //     [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        // ],
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
-                model: Forum_Comment,
+                model: Comment,
                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
                 include: {
                     model: User,
@@ -31,7 +31,7 @@ router.get('/', (req, res) => {
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
 
-            res.render('homepage', {
+            res.render('forum-homepage', {
                 posts,
                 loggedIn: req.session.loggedIn
             });
@@ -44,17 +44,17 @@ router.get('/', (req, res) => {
 
 // get single post
 router.get('/post/:id', (req, res) => {
-    Forum_Post.findOne({
+    Post.findOne({
         where: {
             id: req.params.id
         },
-        // attributes: [
-        //     'id',
-        //     'post_content',
-        //     'title',
-        //     'created_at',
-        //     [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        // ],
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
                 model: Forum_Comment,
@@ -89,13 +89,13 @@ router.get('/post/:id', (req, res) => {
         });
 });
 
-// router.get('/login', (req, res) => {
-//     if (req.session.loggedIn) {
-//         res.redirect('/');
-//         return;
-//     }
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
 
-//     res.render('login');
-// });
+    res.render('login');
+});
 
 module.exports = router;
