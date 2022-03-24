4272c5e7a8dd194580ee6f9645da2238122a3f7d
BullMooseDev <koleervine@gmail.com>
2022-03-23 19:11:22 -0600

    route file format changed

--- controllers/forum-dashboard-routes.js
+++ controllers/forum-dashboard-routes.js
@@ -3,45 +3,7 @@ const sequelize = require('../config/connection');
 const { Forum_Post, User, Forum_Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-// get all posts for dashboard
-router.get('/', withAuth, (req, res) => {
-    console.log(req.session);
-    console.log('======================');
-    Post.findAll({
-        where: {
-            user_id: req.session.user_id
-        },
-        // attributes: [
-        //     'id',
-        //     'post_content',
-        //     'title',
-        //     'created_at',
-        //     [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        // ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('dashboard', { posts, loggedIn: true });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
+
 
 router.get('/edit/:id', withAuth, (req, res) => {
     // find by primary key
@@ -84,5 +46,47 @@ router.get('/edit/:id', withAuth, (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// get all posts for dashboard
+router.get('/', withAuth, (req, res) => {
+    console.log(req.session);
+    console.log('======================');
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        // attributes: [
+        //     'id',
+        //     'post_content',
+        //     'title',
+        //     'created_at',
+        //     [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        // ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('dashboard', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// get route for * catch all to send 404
 
 module.exports = router;
--- controllers/forum-home-routes.js
+++ controllers/forum-home-routes.js
@@ -2,46 +2,6 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Forum_Post, User, Forum_Comment, Forum_Vote } = require('../models');
 
-// get all posts for homepage
-router.get('/', (req, res) => {
-    console.log('======================');
-    Post.findAll({
-        attributes: [
-            'id',
-            'post_content',
-            'title',
-            'created_at',
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
-
-            res.render('forum-homepage', {
-                posts,
-                loggedIn: req.session.loggedIn
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
 // get single post
 router.get('/post/:id', (req, res) => {
     Post.findOne({
@@ -97,5 +57,47 @@ router.get('/login', (req, res) => {
 
     res.render('login');
 });
+
+// get all posts for homepage
+router.get('/forum', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+
+            res.render('forum-homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// catch all route for get * 404
 
 module.exports = router;
