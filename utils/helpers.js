781f2c6914edffe9de17dd2b5203e8418ff01890
john.a.clark3 <john.a.clark3@gmail.com>
2022-03-28 11:18:27 -0600

    made a helper to limit the display of the forum text

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,4 +4,15 @@ module.exports = {
       date
     ).getDate()}/${new Date(date).getFullYear()}`;
   },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+
+    return word;
+  },
+  post_tease: (str) => {
+    const first25 = str.slice(0, 100);
+    return first25;
+  },
 };
