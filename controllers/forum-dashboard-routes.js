0eee337cc1dba44ca69ac69d804383b16005d5b2
BullMooseDev <koleervine@gmail.com>
2022-03-27 20:34:39 -0600

    created proper paths for dashboard and proper css

--- controllers/forum-dashboard-routes.js
+++ controllers/forum-dashboard-routes.js
@@ -27,7 +27,7 @@ router.get("/", withAuth, (req, res) => {
   })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("dashboard", { posts, loggedIn: true });
+      res.render("forum-dashboard", { posts, loggedIn: true });
     })
     .catch((err) => {
       console.log(err);
@@ -101,7 +101,7 @@ router.get('/', withAuth, (req, res) => {
     })
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('dashboard', { posts, loggedIn: true });
+            res.render('forum-dashboard', { posts, loggedIn: true });
         })
         .catch(err => {
             console.log(err);
