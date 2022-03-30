82345d691997ac603e87bdd82292e1828da9ccb9
john.a.clark3 <john.a.clark3@gmail.com>
2022-03-30 11:15:50 -0600

    Testing time outs

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
   secret: "Super secret secret",
-  cookie: {},
+  cookie: { expires: 60000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
